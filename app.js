9325b10893abc3989d66272e0f2ac211d530bbf2
游政諺 <40371231H@users.noreply.github.com>
2018-03-13 20:22:31 +0800

    find collection

--- app.js
+++ app.js
@@ -7,11 +7,22 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'school';
 
 // url : connect url, function(...){...} : callbackfunction
-MongoClient.connect(url, (err, client) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+MongoClient.connect(url, (error, client) => {
+    if (error) {
+        console.log('資料庫連接失敗');
+    } else {
+        console.log('資料庫連接成功');
+    }
 
-  const db = client.db(dbName);
+    //use dbName
+    const db = client.db(dbName);
+    db.collection('student', (error, collection) => {
+        if (error) {
+            console.log('資料庫內無名為 student 的 collection');
+        } else {
+            console.log('資料庫內有名為 student 的 collection');
+        }
+    });
 
-  client.close();
+    client.close();
 });
