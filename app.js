1d416e86b1481e48dc851360d908c418ece1765f
游政諺 <40371231H@users.noreply.github.com>
2018-03-13 21:02:35 +0800

    insert collection

--- app.js
+++ app.js
@@ -19,21 +19,35 @@ MongoClient.connect(url, (error, client) => {
     db.collection('student', (error, collection) => {
         if (error) {
             console.log('資料庫內無名為 student 的 collection');
-        } else {
-            console.log('資料庫內有名為 student 的 collection');
-            collection.find().toArray((error, docs) => {
-                if (error) {
-                    console.log('查詢 student 資料失敗');
-                    return;
-                }
-                for (let i = 0; i < docs.length; i++) {
-                    console.log(docs[i]);
-                }
-                // 確定動作完畢才可將 client 關閉
-                client.close();
-                console.log('資料庫中斷連線');
-            });
-
         }
+
+        var doc = {
+            "學號": "S003",
+            "班別": "CSX2003",
+            "姓名": "王五",
+            "隨機碼": Math.floor(Math.random() * 100 + 1)
+        }
+
+        collection.insertOne(doc, (error, result) => {
+            if (error) {
+                console.log('資料插入失敗');
+                return;
+            }
+            console.log('插入 ' + result.insertedCount + ' 筆資料');
+        });
+        collection.find().toArray((error, docs) => {
+            if (error) {
+                console.log('查詢 student 資料失敗');
+                return;
+            }
+            for (let i = 0; i < docs.length; i++) {
+                console.log(docs[i]);
+            }
+            // 確定動作完畢才可將 client 關閉
+            client.close();
+            console.log('資料庫中斷連線');
+        });
+
+
     });
 });
